c8a279e1be7501c7c256fc2c091901b5cbf7a137
Paul Kristapovich <paulkristapovich@gmail.com>
2017-09-23 18:55:11 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,4 @@
-def my_each(array) # put argument(s) here
-  i = 0
-  while i <= array.length
-    yield
-    i = i + 1
-  end
 
-  my_method do
-    puts array[i]
-  end
-
-  return array
+my_each(collection) do |i|
+  puts i
 end

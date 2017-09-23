4227a8d6ee0653bc548e8271fcf7ea6aad87def4
Paul Kristapovich <paulkristapovich@gmail.com>
2017-09-23 18:46:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  i = 0
+  while i <= array.length
+    yield
+    i = i + 1
+  end
+
+  my_method do
+    array[i]
+  end
+
 end
